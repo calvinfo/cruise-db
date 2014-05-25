a499197a9eede6acaf7847909c53f91092afee4c
Calvin French-Owen <calvin@calv.info>
2014-05-25 14:09:28 +1000

    splitting tests, updating with api fixes

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ var test = require('supertest');
 var assert = require('assert');
 var after = require('after');
 
-describe('cruise-db', function(){
+describe('simple cruise-db', function(){
   var app;
   var server;
   beforeEach(function(done){
@@ -21,14 +21,13 @@ describe('cruise-db', function(){
       var cruise = server.cruise;
       test(app)
         .post('/join')
-        .send({ host: '127.0.0.1', port: 4030 })
+        .send({ addr: '127.0.0.1:4030' })
         .expect({
-          host: cruise.host(),
-          port: cruise.port()
+          addr: cruise.addr(),
         })
         .end(function(err){
           assert(!err);
-          assert.equal(cruise.peers().length, 2);
+          assert.equal(cruise.peers().length, 1);
           done();
         });
     });
@@ -60,60 +59,60 @@ describe('cruise-db', function(){
         .end(done);
     });
   });
+});
 
-  describe('clustering', function(){
-    var servers = [];
+describe('clustering cruise-db', function(){
+  var servers = [];
 
-    before(function(done){
-      for (var i = 0; i < 3; i++) servers.push(new Server());
-      var nodes = servers.map(function(server){ return server.cruise; });
-      nodes.forEach(function(node){
-        nodes.forEach(function(peer){
-          node.addPeer(peer.host(), peer.port());
-        });
+  before(function(done){
+    for (var i = 0; i < 3; i++) servers.push(new Server());
+    var nodes = servers.map(function(server){ return server.cruise; });
+    nodes.forEach(function(node){
+      nodes.forEach(function(peer){
+        node.addPeer(peer.addr());
+      });
+    });
+
+    setTimeout(done, 1000);
+  });
+
+  it('should record a key from the master', function(done){
+    var leader = findLeader();
+    done = after(3, done);
+    test(leader.app())
+      .post('/db/key')
+      .send({ foo: 'bar' })
+      .expect(200)
+      .end(function(err){
+        assert(!err);
+        setTimeout(verify, 300);
       });
 
-      setTimeout(done, 1000);
-    });
-
-    it('should record a key from the master', function(done){
-      var leader = findLeader();
-      done = after(3, done);
-      test(leader.app())
-        .post('/db/key')
-        .send({ foo: 'bar' })
-        .expect(200)
-        .end(function(err){
-          assert(!err);
-          setTimeout(verify, 300);
-        });
-
-      function verify(){
-        servers.forEach(function(server){
-          test(server.app())
-            .get('/db/key')
-            .expect({ foo: 'bar' })
-            .end(done);
-        });
-      }
-    });
-
-    /**
-     * Finds the leader from the group of servers
-     *
-     * @return {Server} leader
-     */
-
-    function findLeader(){
-      var leaders = servers
-        .filter(function(server){
-          return server.isLeader();
-        })
-        .sort(function(x, y){
-          return x.cruise.term() < y.cruise.term();
-        });
-
-      return leaders[0];
+    function verify(){
+      servers.forEach(function(server){
+        test(server.app())
+          .get('/db/key')
+          .expect({ foo: 'bar' })
+          .end(done);
+      });
     }
   });
+
+  /**
+   * Finds the leader from the group of servers
+   *
+   * @return {Server} leader
+   */
+
+  function findLeader(){
+    var leaders = servers
+      .filter(function(server){
+        return server.isLeader();
+      })
+      .sort(function(x, y){
+        return x.cruise.term() < y.cruise.term();
+      });
+
+    return leaders[0];
+  }
 });
